bbf2d2b37f821867c56d643eb6adc3cc802e0306
Tha1n <alx.quettier@gmail.com>
2015-02-27 11:31:31 +0100

    Ajout methode estFranchissable

--- Assets/Scripts/Model/Terrain_Decor/Decor.cs
+++ Assets/Scripts/Model/Terrain_Decor/Decor.cs
@@ -3,10 +3,15 @@ using System.Collections;
 
 public class Decor : MonoBehaviour {
 
-    private string nom;
-    private string description;
+    private string 		nom;
+    private string 		description;
+	private bool		estFranch;
 
 
+	public bool estFranchissable ()
+	{
+		return this.estFranch;
+	}
 }
 
 
