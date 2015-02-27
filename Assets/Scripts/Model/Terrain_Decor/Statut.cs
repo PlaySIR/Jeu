40cd17478c07bb8440fa94b6f6e8fc1f9feb5747
Tha1n <alx.quettier@gmail.com>
2015-02-27 11:01:46 +0100

    Ajout du namespace Statut, de la classe statique StatutConstants, constructeur et getter de Statut

--- Assets/Scripts/Model/Terrain_Decor/Statut.cs
+++ Assets/Scripts/Model/Terrain_Decor/Statut.cs
@@ -1,13 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Statut : MonoBehaviour {
+namespace Statut
+{
 
+	static public class StatutConstants
+	{
+		/// <summary>
+		/// The value of OFFENSIVE tile.
+		/// </summary>
+		public const uint OFFENSIF 	= 2;
+		/// <summary>
+		/// The value of DEFENSIVE tile.
+		/// </summary>
+		public const uint DEFENSIF 	= 1;
+		/// <summary>
+		/// The value of NEUTRAL tile.
+		/// </summary>
+		public const uint NEUTRE 	= 0;
+	}
 
-    private int statut;
-    
-    public static int OFFENSIF, DEFENSIF, NEUTRE;
+	public class Statut : MonoBehaviour {
 
+		/// <summary>
+		/// The statut.
+		/// </summary>
+	    private uint statut;
+	    
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Statut.Statut"/> class.
+		/// </summary>
+		/// <param name="statutT">Temporary Statut, set to neutral by default.</param>
+		public Statut(uint statutT = StatutConstants.NEUTRE)
+		{
+			this.statut = statutT;
+		}
 
+		/// <summary>
+		/// Gets the statut.
+		/// </summary>
+		/// <returns>The statut.</returns>
+		public uint getStatut()
+		{
+			return this.statut;
+		}
+
+	}
 
 }
