3415bca317d64a30cd1b39fbc205e9305c13f47e
steven <steven.pescheteau@gmail.com>
2015-02-27 11:48:27 +0100

    Ajout de la classe Objet

--- Assets/Scripts/Model/Objets/Objet.cs
+++ Assets/Scripts/Model/Objets/Objet.cs
@@ -3,20 +3,27 @@ using System.Collections;
 
 public abstract class Objet : MonoBehaviour {
 
-    private string nom;
-    private string description;
-
-
-
-
-
-	// Use this for initialization
-	void Start () {
-	
+	private string _nom;
+	public string Nom {
+		get {
+			return _nom;
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+    private string _description;
+	public string Description {
+		get {
+			return _description;
+		}
+	}
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="Objet"/> class.
+	/// </summary>
+	/// <param name="nom">Nom.</param>
+	/// <param name="description">Description.</param>
+	public Objet (string nom, string description){
+		this._nom = nom;
+		this._description = description;
 	}
 }
