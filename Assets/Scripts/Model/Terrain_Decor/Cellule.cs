51574a09b94be7bcde0c806f5db788e9845d4d90
Tha1n <alx.quettier@gmail.com>
2015-02-27 11:30:20 +0100

    Ajout Constructeur, getter

--- Assets/Scripts/Model/Terrain_Decor/Cellule.cs
+++ Assets/Scripts/Model/Terrain_Decor/Cellule.cs
@@ -2,15 +2,79 @@
 using System.Collections;
 
 public class Cellule : MonoBehaviour {
-
+	/// <summary>
+	/// The statut.
+	/// </summary>
     private Statut 			statut;
+	/// <summary>
+	/// The coordonnee.
+	/// </summary>
     private Coordonnee		coordonnee;
+	/// <summary>
+	/// The decor.
+	/// </summary>
     private Decor			decor;
-    private Team			appartenance;
+	/// <summary>
+	/// The equipe appartenance.
+	/// </summary>
+    private Team			equipeAppartenance;
 
+	/// <summary>
+	/// Initializes a new instance of the <see cref="Cellule"/> class.
+	/// </summary>
+	/// <param name="statT">Temporary Statut.</param>
+	/// <param name="coordT">Temporary Coordinate.</param>
+	/// <param name="decT">Temporary Decor.</param>
+	/// <param name="teamT">Temporary Team.</param>
+	public Cellule(Statut statT, Coordonnee coordT, Decor decT, Team teamT)
+		: base()
+	{
+		this.statut 				= statT;
+		this.coordonnee 			= coordT;
+		this.decor 					= decT;
+		this.equipeAppartenance		= teamT;
+	}
 
+	/// <summary>
+	/// Gets the statut.
+	/// </summary>
+	/// <returns>The statut.</returns>
+	public Statut getStatut()
+	{
+		return this.statut;
+	}
+
+	/// <summary>
+	/// Gets the coordonnee.
+	/// </summary>
+	/// <returns>The coordonnee.</returns>
+	public Coordonnee getCoordonnee()
+	{
+		return this.coordonnee;
+	}
+	/// <summary>
+	/// Gets the decor.
+	/// </summary>
+	/// <returns>The decor.</returns>
+	public Decor getDecor()
+	{
+		return this.decor;
+	}
+	/// <summary>
+	/// Gets the team.
+	/// </summary>
+	/// <returns>The team.</returns>
+	public Team getTeam()
+	{
+		return this.equipeAppartenance;
+	}
+
+	/// <summary>
+	/// Determine if the <c>Cellule</c> is passable.
+	/// </summary>
+	/// <returns><c>true</c>, if franchissable was real, <c>false</c> otherwise.</returns>
     public bool estFranchissable()
     {
-        return false;
+        return this.decor.estFranchissable();
     }
 }
