0ab1e4087105e69ce5d7caddcb1b7ea6b1f2c307
steven <steven.pescheteau@gmail.com>
2015-02-27 21:33:12 +0100

    Inventaire terminé ! Utilisable donc. Objet terminé, Magasin et Joueur en cours. Formalisation de ces classes.

--- Assets/Scripts/Model/Objets/Magasin.cs
+++ Assets/Scripts/Model/Objets/Magasin.cs
@@ -2,38 +2,42 @@
 using System.Collections;
 using System.Collections.Generic;
 
-
+/// <summary>
+/// @Author: Steven Pescheteau
+/// </summary>
 public class Magasin : MonoBehaviour {
 
+	#region Attributs
     private string _nom;
-	public string Nom {
-		get { return _nom;}
-	}
+	public string Nom { get { return _nom;} }
 
     private Inventaire _inventaire;
-	public Inventaire Inventaire{
-		get { return _inventaire; }
-	}
+	public Inventaire Inventaire{ get { return _inventaire; } }
 
 	private double _tarifApplique;
-	public double TarifApplique{
-		get { return _tarifApplique; }
-	}
+	public double TarifApplique{ get { return _tarifApplique; } }
+	#endregion
 
 	/// <summary>
-	/// Initializes a new instance of the <see cref="Magasin"/> class.
+	/// Magasin contenant une liste d'objet à des prix plus ou moins alléchants
 	/// </summary>
 	/// <param name="nom">Nom.</param>
-	/// <param name="objets">Objets.</param>
-	public Magasin(string nom, Inventaire inventaire, double tarifApplique){
+	/// <param name="inventaire">Inventaire.</param>
+	/// <param name="tarifApplique">Tarif applique.</param>
+	public Magasin(string nom, Inventaire inventaire, double tarifApplique)
+	{
 		this._nom = nom;
 		this._inventaire = inventaire;
 		this._tarifApplique = tarifApplique;
 	}
 
-    public Objet vendre() {
-		//TODO
+    public void vendre(Objet objet) 
+	{
+		//TODO Player doit etre codé.
     }
 
+	public boolean acheter(Objet objet)
+	{
+		//TODO Player doit etre codé.
 	}
 }
--- Assets/Scripts/Model/Objets/Objet.cs
+++ Assets/Scripts/Model/Objets/Objet.cs
@@ -1,26 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// @Author: Steven Pescheteau
+/// </summary>
 public abstract class Objet : MonoBehaviour {
 
+	#region Attributs
 	private string _nom;
-	public string Nom {
-		get { return _nom; }
-	}
+	public string Nom { get { return _nom; } }
 
     private string _description;
-	public string Description {
-		get {
-			return _description;
-		}
-	}
+	public string Description { get { return _description; } }
 
 	private int _prix;
-	public int Prix {
-		get {
-			return _prix;
-		}
-	}
+	public int Prix { get { return _prix; } }
+	#endregion
 
 	/// <summary>
 	/// Initializes a new instance of the <see cref="Objet"/> class.
