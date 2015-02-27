fab15a4ebd081d7f9f69173b4ccad1a637f6d2c6
Tha1n <alx.quettier@gmail.com>
2015-02-27 20:56:16 +0100

    Ajout de la région Getters et Methods + Indication TODO

--- Assets/Scripts/Model/Terrain_Decor/Decor.cs
+++ Assets/Scripts/Model/Terrain_Decor/Decor.cs
@@ -7,11 +7,28 @@ public class Decor : MonoBehaviour {
     private string 		description;
 	private bool		estFranch;
 
+	//TODO
 
+
+
+
+	//TODO
+	#region Decor Getters
+
+
+	#endregion
+
+	#region Decor Methods
+	/// <summary>
+	/// Ests the franchissable.
+	/// </summary>
+	/// <returns><c>true</c>, if franchissable was ested, <c>false</c> otherwise.</returns>
 	public bool estFranchissable ()
 	{
 		return this.estFranch;
 	}
+
+	#endregion
 }
 
 
