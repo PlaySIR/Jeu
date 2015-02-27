0552031df57b3f88caee94f2e581c653cdccea31
Karkariy <helali44@hotmail.fr>
2015-02-27 14:56:10 +0100

    getter correction

--- Assets/Scripts/Model/AFAIRE_GRP2/Caracteristiques.cs
+++ Assets/Scripts/Model/AFAIRE_GRP2/Caracteristiques.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 using System.Collections;
 
 public class Caracteristiques : MonoBehaviour {
-
-	private uint vitalite;
-    private uint force;
-    private uint defense;
-    private uint initiative;
-    private uint pointMouvement;
-
-		/// <summary>
+	
+	private uint _vitalite;
+	private uint _force;
+	private uint _defense;
+	private uint _initiative;
+	private uint _pointMouvement;
+	
+	/// <summary>
 	/// Initializes a new instance of the <see cref="Caracteristiques"/> class.
 	/// </summary>
 	/// <param name="vitalite">Vitalite.</param>
@@ -18,83 +18,93 @@ public class Caracteristiques : MonoBehaviour {
 	/// <param name="initiative">Initiative.</param>
 	/// <param name="pointMouvement">Point mouvement.</param>
 	public Caracteristiques(uint vitalite, uint force, uint defense, uint initiative, uint pointMouvement){
-		this.vitalite=vitalite;
-		this.force=force;
-		this.defense=defense;
-		this.initiative=initiative;
-		this.pointMouvement=force;
-		}
-
+		this._vitalite=vitalite;
+		this._force=force;
+		this._defense=defense;
+		this._initiative=initiative;
+		this._pointMouvement=force;
+	}
+	
 	/// <summary>
 	/// Gets the vitalite.
 	/// </summary>
 	/// <returns>The vitalite.</returns>
-	uint getVitalite(){
-		return this.vitalite;
+	uint Vitalite{
+		get{
+			return this._vitalite;
+		}
 	}
-
+	
 	/// <summary>
 	/// Gets the force.
 	/// </summary>
 	/// <returns>The force.</returns>
-	uint getForce(){
-		return this.force;
+	uint Force{
+		get{
+			return this._force;
+		}
 	}
-
+	
 	/// <summary>
 	/// Gets the defense.
 	/// </summary>
 	/// <returns>The defense.</returns>
-	uint getDefense(){
-		return this.defense;
+	uint Defense{
+		get{
+			return this._defense;
+		}
 	}
-
+	
 	/// <summary>
 	/// Gets the initiative.
 	/// </summary>
 	/// <returns>The initiative.</returns>
-	uint getInitiative(){
-		return this.initiative;
+	uint Initiative{
+		get{
+			return this._initiative;
+		}
 	}
-
+	
 	/// <summary>
 	/// Gets the point mouvement.
 	/// </summary>
 	/// <returns>The point mouvement.</returns>
-	uint getPointMouvement(){
-		return this.pointMouvement;
+	uint PointMouvement{
+		get{
+			return this._pointMouvement;
+		}
 	}
-
+	
 	/// <summary>
 	/// Adds the vitalite.
 	/// </summary>
 	/// <param name="rajout">Rajout.</param>
 	void addVitalite(uint rajout){
-		this.vitalite+=rajout;
+		this._vitalite+=rajout;
 	}
-
+	
 	/// <summary>
 	/// Adds the force.
 	/// </summary>
 	/// <param name="rajout">Rajout.</param>
 	void addForce(uint rajout){
-		this.force+=rajout;
+		this._force+=rajout;
 	}
-
+	
 	/// <summary>
 	/// Adds the defense.
 	/// </summary>
 	/// <param name="rajout">Rajout.</param>
 	void addDefense(uint rajout){
-		this.defense+=rajout;
+		this._defense+=rajout;
 	}
-
+	
 	/// <summary>
 	/// Adds the initiative.
 	/// </summary>
 	/// <param name="rajout">Rajout.</param>
 	void addInitiative(uint rajout){
-		this.initiative+=rajout;
+		this._initiative+=rajout;
 	}
-
+	
 }
