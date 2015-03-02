8d5aca9765b417fbac8ce301b184b205bce8e27f
Tha1n <alx.quettier@gmail.com>
2015-03-02 09:08:46 +0100

    Implémentation DecorInteractif

--- Assets/Scripts/Model/Terrain_Decor/DecorInteractif.cs
+++ Assets/Scripts/Model/Terrain_Decor/DecorInteractif.cs
@@ -5,12 +5,68 @@ public class DecorInteractif : Decor {
 
     private Consommable consommable;
 
+	/// <summary>
+	/// Initializes a new instance of the <see cref="DecorInteractif"/> class.
+	/// </summary>
+	/// <param name="consoT">Conso t.</param>
+	public DecorInteractif(Consommable consoT)
+		: base()
+	{
+		this.consommable = consoT;
+	}
 
-    public void interagir(Personnage personnage)
-    {
+	/// <summary>
+	/// Initializes a new instance of the <see cref="DecorInteractif"/> class.
+	/// </summary>
+	/// <param name="nomT">Nom t.</param>
+	/// <param name="descriptionT">Description t.</param>
+	/// <param name="estFranchissableT">If set to <c>true</c> est franchissable t.</param>
+	/// <param name="consoT">Conso t.</param>
+	public DecorInteractif(string nomT, string descriptionT,
+	                       bool estFranchissableT, Consommable consoT)
+		: base(nomT, descriptionT, estFranchissableT)
+	{
+		this.consommable = consoT;
+	}
 
-    }
+	/// <summary>
+	/// Initializes a new instance of the <see cref="DecorInteractif"/> class.
+	/// </summary>
+	/// <param name="estFranchissableT">If set to <c>true</c> est franchissable t.</param>
+	/// <param name="consoT">Conso t.</param>
+	public DecorInteractif(bool estFranchissableT, Consommable consoT)
+		: base(estFranchissableT)
+	{
+		this.consommable = consoT;
+
+	}
+
+	#region DecorInteractif Getters
+
+	private Consommable Consommable {
+		get {
+			return consommable;
+		}
+	}
 
 
+	#endregion
 
+	#region DecorInteractif Methods
+
+	/// <summary>
+	/// Gets the consommable.
+	/// </summary>
+	/// <returns>The consommable.</returns>
+	public override Consommable getConsommable ()
+	{
+		return this.Consommable;
+	}
+
+//	public void interagir(Personnage personnage)
+//	{
+//		
+//	}
+
+	#endregion
 }
