d72bc95271396416694c3c241250b28ca64f0a49
Karkariy <helali44@hotmail.fr>
2015-03-26 18:40:00 +0100

    Plugins - class Map

--- Assets/Scripts/Controle/GestionCombat.cs
+++ Assets/Scripts/Controle/GestionCombat.cs
@@ -6,7 +6,7 @@ public class GestionCombat : MonoBehaviour {
 
 	//Attributs
 	private List<Personnage> _personnages;
-	private Team _allie;
+	private Team _equipe;
 	private Team _ennemi;
 	private Cellule [][] _map;
 
@@ -18,7 +18,7 @@ public class GestionCombat : MonoBehaviour {
 	/// <param name="ennemi">Equipe Ennemi.</param>
 	/// <param name="map">Map à générer.</param>
 	public GestionCombat (List<Personnage> perso, Team allie, Team ennemi, Cellule [][] map){
-		this._allie = allie;
+		this._equipe = allie;
 		this._ennemi = ennemi;
 		this._personnages = perso;
 		this._map = map;
